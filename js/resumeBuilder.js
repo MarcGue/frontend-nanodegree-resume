f67f99f11ecb1542c35be305177c22af39be8986
Marc Günnel <marc.guennel@adesso.de>
2017-02-27 13:57:57 +0100

    update resume due to udacity project specification

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,201 +2,186 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 var bio = {
-  "name": "Marc Günnel",
-  "role": "Software Engineer",
-  "contacts": {
-    "mobile": "0151 123456",
-    "email": "marc.guennel@googlemail.com",
-    "github": "MarcGue",
-    "location": "Johann-Krieger-Strasse 3, 90469 Nürnberg"
+  name: 'Marc Günnel',
+  role: 'Software Engineer',
+  contacts:  {
+    mobile: '0151 123456',
+    email: 'marc.guennel@googlemail.com',
+    github: 'MarcGue',
+    location: 'Johann-Krieger-Strasse 3, 90469 Nürnberg'
   },
-  "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc",
-  "skills": [
-    "awesome", "delivering things", "cryogenic sleept", "saving the universe"
+  welcomeMessage: 'Welcome to my Resume',
+  skills: [
+    'HTML', 'CSS', 'JavaScript', 'JQuery'
   ],
-  "bioPic": "images/fry.jpg"
-};
+  bioPic: 'images/fry.jpg',
+  display: function () {
+    var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+    $('#header').prepend(formattedRole);
 
-bio.displayBio = function() {
-  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-  $("#header").prepend(formattedRole);
+    var formattedName = HTMLheaderName.replace('%data%', bio.name);
+    $('#header').prepend(formattedName);
 
-  var formattedName = HTMLheaderName.replace("%data%", bio.name);
-  $("#header").prepend(formattedName);
+    var formattedBioPic = HTMLbioPic.replace('%data%', bio.bioPic);
+    $('#header').append(formattedBioPic);
 
-  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-  $("#header").append(formattedBioPic);
+    var formattedWelcomeMessage = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+    $('#header').append(formattedWelcomeMessage);
 
-  var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-  $("#header").append(formattedWelcomeMessage);
-};
+    $('#header').append(HTMLskillsStart);
+    bio.skills.forEach(function (skill) {
+      $('#skills').append(HTMLskills.replace('%data%', skill));
+    })
 
-bio.displayContacts = function () {
-  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-  $("#topContacts").append(formattedMobile);
+    var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+    $('#topContacts').append(formattedMobile);
+    $('#footerContacts').append(formattedMobile);
 
-  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-  $("#topContacts").append(formattedEmail);
-  var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
-  $("#topContacts").append(formattedGitHub);
+    var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+    $('#topContacts').append(formattedEmail);
+    $('#footerContacts').append(formattedEmail);
 
-  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-  $("#topContacts").append(formattedLocation);
+    var formattedGitHub = HTMLgithub.replace('%data%', bio.contacts.github);
+    $('#topContacts').append(formattedGitHub);
+    $('#footerContacts').append(formattedGitHub);
+
+    var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+    $('#topContacts').append(formattedLocation);
+    $('#footerContacts').append(formattedLocation);
+
+    $('#mapDiv').append(googleMap);
+  }
 };
 
 var work = {
   jobs: [{
-    employer: "adesso AG",
-    title: "Software Engineer",
-    dates: "01.10.2016 - now",
-    location: "Am Tullnaupark 15, 90402 Nürnberg",
-    description: ""
+    employer: 'adesso AG',
+    title: 'Software Engineer',
+    dates: '01.10.2016 - now',
+    location: 'Am Tullnaupark 15, 90402 Nürnberg',
+    description: 'Developing Software in different projects for different customers'
   }, {
-    employer: "Peak Solution GmbH",
-    title: "Software Engineer",
-    dates: "01.02.2011 - 30.09.2016",
-    location: "Lina-Ammon-Straße 22, 90471 Nürnberg",
-    description: ""
+    employer: 'Peak Solution GmbH',
+    title: 'Software Engineer',
+    dates: '01.02.2011 - 30.09.2016',
+    location: 'Lina-Ammon-Straße 22, 90471 Nürnberg',
+    description: 'Developing Software especially for measurement data systems'
   }],
-};
+  display: function () {
+    work.jobs.forEach(function (job) {
+      $('#workExperience').append(HTMLworkStart);
 
-work.displayWork = function() {
-  work.jobs.forEach(function(job) {
-    $("#workExperience").append(HTMLworkStart);
+      var formattedWorkEmployer = HTMLworkEmployer.replace('%data%', job.employer);
+      var formattedWorkTitle = HTMLworkTitle.replace('%data%', job.title);
+      var formattedWorkEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
+      $('.work-entry:last').append(formattedWorkEmployerTitle);
 
-    var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", job.employer);
-    var formattedWorkTitle = HTMLworkTitle.replace("%data%", job.title);
-    var formattedWorkEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
-    $(".work-entry:last").append(formattedWorkEmployerTitle);
+      var formattedWorkDates = HTMLworkDates.replace('%data%', job.dates);
+      $('.work-entry:last').append(formattedWorkDates);
 
-    var formattedWorkDates = HTMLworkDates.replace("%data%", job.dates);
-    $(".work-entry:last").append(formattedWorkDates);
-
-    var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
-    $(".work-entry:last").append(formattedDescription);
-  });
+      var formattedDescription = HTMLworkDescription.replace('%data%', job.description);
+      $('.work-entry:last').append(formattedDescription);
+    });
+  }
 };
 
 var projects = {
   projects: [{
-    title: "Portfolio",
-    dates: "10.02.2017 - 20.02.2017",
-    description: "Build a Portfolio",
+    title: 'Portfolio',
+    dates: '10.02.2017 - 20.02.2017',
+    description: 'Build a Portfolio',
     images: [
-      "images/fry.jpg"
+      'images/fry.jpg'
     ]
-  }]
-};
+  }, {
+    title: 'Resume',
+    dates: '10.02.2017 - 20.02.2017',
+    description: 'Build a Resume',
+    images: [
+      'images/fry.jpg',
+      'images/fry.jpg'
+    ]
+  }],
+  display: function () {
+    projects.projects.forEach(function (project) {
+      $('#projects').append(HTMLprojectStart);
 
-projects.displayProjects = function() {
-  projects.projects.forEach(function(project) {
-    $("#projects").append(HTMLprojectStart);
+      var formattedProjectTitle = HTMLprojectTitle.replace('%data%', project.title);
+      $('.project-entry:last').append(formattedProjectTitle);
 
-    var formattedProjectTitle = HTMLprojectTitle.replace("%data%", project.title);
-    $(".project-entry:last").append(formattedProjectTitle);
+      var formattedProjectDates = HTMLprojectDates.replace('%data%', project.dates);
+      $('.project-entry:last').append(formattedProjectDates);
 
-    var formattedProjectDates = HTMLprojectDates.replace("%data%", project.dates);
-    $(".project-entry:last").append(formattedProjectDates);
+      var formattedProjectDescription = HTMLprojectDescription.replace('%data%', project.description);
+      $('.project-entry:last').append(formattedProjectDescription);
 
-    var formattedProjectDescription = HTMLprojectDescription.replace("%data%", project.description);
-    $(".project-entry:last").append(formattedProjectDescription);
-
-    if (project.images.length > 0) {
-      project.images.forEach(function(image) {
-        var formattedProjectImage = HTMLprojectImage.replace("%data%", image);
-        $(".project-entry:last").append(formattedProjectImage);
-      });
-    }
-  });
+      if (project.images.length > 0) {
+        project.images.forEach(function (image) {
+          var formattedProjectImage = HTMLprojectImage.replace('%data%', image);
+          $('.project-entry:last').append(formattedProjectImage);
+        });
+      }
+    });
+  }
 };
 
 var education = {
   schools: [{
-    name: "Deutsche Telekom AG",
-    degree: "IT-Systemkaufmann",
-    dates: "01.09.2007 - 31.07.2010",
-    location: "Hansastraße 7A, 90441 Nürnberg",
-    major: "IT-Systemkaufmann"
+    name: 'Deutsche Telekom AG',
+    degree: 'IT-Systemkaufmann',
+    dates: '01.09.2007 - 31.07.2010',
+    location: 'Hansastraße 7A, 90441 Nürnberg',
+    major: 'IT-Systemkaufmann'
   }, {
-    name: "Peter-Vischer-Gymnasium",
-    degree: "Oberstufenreife",
-    dates: "01.09.1992 - 01.09.2007",
-    location: "Bielingplatz 2, 90419 Nürnberg",
-    major: "Wirtschaft & Chemie"
-  }]
+    name: 'Peter-Vischer-Gymnasium',
+    degree: 'Oberstufenreife',
+    dates: '01.09.1992 - 01.09.2007',
+    location: 'Bielingplatz 2, 90419 Nürnberg',
+    major: 'Wirtschaft & Chemie'
+  }],
+  onlineCourses: [{
+    title: 'Front-Ent Web Developer Nanodegree',
+    school: 'Udacity',
+    dates: '10.02.2017 - now',
+    url: 'https://de.udacity.com/course/front-end-web-developer-nanodegree--nd001/'
+  }],
+  display: function () {
+    education.schools.forEach(function (school) {
+      $('#education').append(HTMLschoolStart);
+
+      var formattedSchoolName = HTMLschoolName.replace('%data%', school.name);
+      var formattedSchoolDegree = HTMLschoolDegree.replace('%data%', school.degree);
+      $('.education-entry:last').append(formattedSchoolName + formattedSchoolDegree);
+
+      var formattedSchoolDates = HTMLschoolDates.replace('%data%', school.dates);
+      $('.education-entry:last').append(formattedSchoolDates);
+
+      var formattedSchoolLocation = HTMLschoolLocation.replace('%data%', school.location);
+      $('.education-entry:last').append(formattedSchoolLocation);
+
+      var formattedSchoolMajor = HTMLschoolMajor.replace('%data%', school.major);
+      $('.education-entry:last').append(formattedSchoolMajor);
+    });
+
+    $('#education').append(HTMLonlineClasses);
+
+    education.onlineCourses.forEach(function (onlineClass) {
+      $('#education').append(HTMLschoolStart);
+
+      var formattedClassTitle = HTMLonlineTitle.replace('%data%', onlineClass.title);
+      var formattedClassSchool = HTMLonlineSchool.replace('%data%', onlineClass.school);
+      $('.education-entry:last').append(formattedClassTitle + formattedClassSchool);
+
+      var formattedClassDates = HTMLonlineDates.replace('%data%', onlineClass.dates);
+      $('.education-entry:last').append(formattedClassDates);
+
+      var formattedClassURL = HTMLonlineURL.replace('%data%', onlineClass.url);
+      $('.education-entry:last').append(formattedClassURL);
+    });
+  }
 };
 
-education.displaySchools = function() {
-  education.schools.forEach(function(school) {
-    $("#education").append(HTMLschoolStart);
-
-    var formattedSchoolName = HTMLschoolName.replace("%data%", school.name);
-    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
-    $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
-
-    var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
-    $(".education-entry:last").append(formattedSchoolDates);
-
-    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
-    $(".education-entry:last").append(formattedSchoolLocation);
-
-    var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", school.major);
-    $(".education-entry:last").append(formattedSchoolMajor);
-  });
-};
-
-var onlineClasses = {
-  onlineClasses: [{
-    title: "Front-Ent Web Developer Nanodegree",
-    school: "Udacity",
-    dates: "10.02.2017 - now",
-    url: "https://de.udacity.com/course/front-end-web-developer-nanodegree--nd001/"
-  }]
-};
-
-onlineClasses.displayClasses = function() {
-  $("#education").append(HTMLonlineClasses);
-
-  onlineClasses.onlineClasses.forEach(function(onlineClass) {
-    $("#education").append(HTMLschoolStart);
-
-    var formattedClassTitle = HTMLonlineTitle.replace("%data%", onlineClass.title);
-    var formattedClassSchool = HTMLonlineSchool.replace("%data%", onlineClass.school);
-    $(".education-entry:last").append(formattedClassTitle + formattedClassSchool);
-
-    var formattedClassDates = HTMLonlineDates.replace("%data%", onlineClass.dates);
-    $(".education-entry:last").append(formattedClassDates);
-
-    var formattedClassURL = HTMLonlineURL.replace("%data%", onlineClass.url);
-    $(".education-entry:last").append(formattedClassURL);
-  });
-};
-
-bio.displayBio();
-bio.displayContacts();
-work.displayWork();
-projects.displayProjects();
-education.displaySchools();
-onlineClasses.displayClasses();
-
-function locationizer(work) {
-  var locations = [];
-  work.jobs.forEach(function(job) {
-    locations.push(job.location);
-  });
-
-  return locations;
-}
-
-function inName(name) {
-  name = name.trim().split(" ");
-  console.log(name);
-  name[1] = name[1].toUpperCase();
-  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
-
-  return name[0] + " " + name[1];
-}
-
-$("#main").append(internationalizeButton);
-
-$("#mapDiv").append(googleMap);
+bio.display();
+work.display();
+projects.display();
+education.display();
